15c95317d5a8425fc9afb3f2082124b417a5662e
andrii <a.tata4.26@gmail.com>
2024-09-27 23:04:40 +0300

    fx2

--- api/dbConnect.js
+++ api/dbConnect.js
@@ -1,26 +1,26 @@
 const MONGODB_URI = process.env.MONGODB_URI;
 import { MongoClient, ServerApiVersion } from 'mongodb';
 
-const client = new MongoClient(MONGODB_URI, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+// const client = new MongoClient(MONGODB_URI, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
 
 if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable', MONGODB_URI, process.env.MONGODB_URI);
