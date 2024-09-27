9a44d49db8048f39d3c1a0588e271a686f309e18
andrii <a.tata4.26@gmail.com>
2024-09-27 22:54:18 +0300

    fx

--- api/dbConnect.js
+++ api/dbConnect.js
@@ -1,56 +1,6 @@
-// import { MongoClient } from 'mongodb';
-// const uri = 'mongodb+srv://atata426:qhqfN8Ai0Qrr0hm@jmbrld.zbxwy.mongodb.net/?retryWrites=true&w=majority';
-// let cachedClient = null;
-
-// export async function connectToDatabase() {
-//   if (cachedClient) {
-//     return cachedClient;
-//   }
-
-//   const client = await MongoClient.connect(uri, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   });
-
-//   cachedClient = client;
-//   return client;
-// }
-
-//import { MongoClient } from 'mongodb';
-
 const { MONGODB_URI } = process.env.MONGODB_URI;
-
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://atata426:Uv1Jo4R85Ag9RAWe@jmbrld.zbxwy.mongodb.net/?retryWrites=true&w=majority&appName=jmbrld";
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-// async function run() {
-//   try {
-//     // Connect the client to the server  (optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
-//const MONGODB_URI = 'mongodb+srv://atata426:Uv1Jo4R85Ag9RAWe@jmbrld.zbxwy.mongodb.net/?retryWrites=true&w=majority&appName=jmbrld';
-const MONGODB_URI1 = "mongodb+srv://atata426:Uv1Jo4R85Ag9RAWe@jmbrld.zbxwy.mongodb.net/?retryWrites=true&w=majority&appName=jmbrld";
-
 import { MongoClient, ServerApiVersion } from 'mongodb';
 
-const uri = "mongodb+srv://atata426:<db_password>@jmbrld.zbxwy.mongodb.net/?retryWrites=true&w=majority&appName=jmbrld";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -73,7 +23,7 @@ async function run() {
 run().catch(console.dir);
 
 if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable');
+  throw new Error('Please define the MONGODB_URI environment variable', MONGODB_URI, process.env.MONGODB_URI);
 }
 
 let cachedClient = null;
